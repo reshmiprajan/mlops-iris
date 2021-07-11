b526cbeba6baad7d52868bfbdf130a7675af512d
reshmiprajan <saphirecad@gmail.com>
2021-07-11 21:35:58 +0530

    [ Added testcases,timestamp and classifiers]

--- test_app.py
+++ test_app.py
@@ -11,6 +11,32 @@ def test_ping():
 
 
 # test to check if Iris Virginica is classified correctly
+def test_pred_versicolor():
+    # defining a sample payload for the testcase
+    payload1 = {
+        "sepal_length": 7,
+        "sepal_width": 3.2,
+        "petal_length": 4.7,
+        "petal_width": 1.4,
+    }
+     with TestClient(app) as client:
+        response = client.post("/predict_flower", json=payload)
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"flower_class": "Iris Versicolor"}
+def test_pred_setosa():
+    # defining a sample payload for the testcase
+    payload = {
+        "sepal_length": 5.1,
+        "sepal_width": 3.5,
+        "petal_length": 1.4,
+        "petal_width": .2,
+    }
+    with TestClient(app) as client:
+        response = client.post("/predict_flower", json=payload)
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"flower_class": "Iris Sentosa"}
 def test_pred_virginica():
     # defining a sample payload for the testcase
     payload = {
